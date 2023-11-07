c99b7d465816ffe1bf2e372a8b77a2e659283922
Jaewon Hur <hurjaewon@snu.ac.kr>
2023-11-07 19:44:30 +0900

    Add control_arduino

--- picante.py
+++ picante.py
@@ -3,6 +3,7 @@
 import openai
 import requests, json
 import click, threading, queue, pyaudio, audioop, time, io, signal, sys, os
+import serial
 import sounddevice as sd
 import soundfile as sf
 import numpy as np
@@ -11,10 +12,12 @@ from math import log10
 
 ################### Global Parameters #########################################
 
-SOUND_THRESHOLD = -45
-MAX_BINS=1000
+SOUND_THRESHOLD = -60
+MAX_BINS=300
 DUMP_INTERVAL=0.5
 MAX_SOUND_LENGTH = 10
+
+TTY = '/dev/ttyUSB0'
 
 ###############################################################################
 
@@ -30,6 +33,8 @@ CONVERT = queue.Queue()
 CONTROL = queue.Queue()
 
 SAMPLE_RATE=16000
+BAUDRATE = 9600
+TIMEOUT=1
 
 class MutableNumber:
     def __init__(self, val):
@@ -209,7 +214,6 @@ def convert_sound(src: queue.Queue, dest: queue.Queue,
     transcribe = TRANSCRIBERS[transcriber]
 
     def go_transcribe(sounds: np.array):
-
         out = io.BytesIO()
         out.name = 'out.wav'
 
@@ -228,7 +232,6 @@ def convert_sound(src: queue.Queue, dest: queue.Queue,
           f'        using {transcriber}\n')
     CONTROL.put(True)
 
-    TIMEOUT=1
     transcribe_threads = []
     while not stopped:
         try:
@@ -253,10 +256,79 @@ def convert_sound(src: queue.Queue, dest: queue.Queue,
 
 
 def control_arduino(src: queue.Queue, debug: str):
-    CONTROL.get()
 
+    dprint = dprint_constructor('control', debug)
+
+    CHOSUNG_LIST = [
+        'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 
+        'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 
+        'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 
+        'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 
+        'ㅌ', 'ㅍ', 'ㅎ',
+    ]
+
+    CHOSUNG_TO_LIGHT_NUM = {
+        "ㅅ": 6,
+        "ㅌ": 1,
+        "ㅋ": 2,
+        "ㅍ": 3,
+        "ㅎ": 4,
+        "ㅊ": 5,
+        "ㄹ": 0,
+        "ㄴ": 7,
+        "ㄷ": 8,
+        "ㅂ": 9,
+        "ㄱ": 10,
+        "ㅇ": 11,
+        "ㅁ": 12,
+        "ㅈ": 13,
+        "ㅃ": 9,
+        "ㅆ": 6,
+        "ㄲ": 10,
+        "ㅉ": 13,
+    }
+
+    HIGH_FREQ_CHOSUNGS = [
+        'ㅅ', 'ㅋ', 'ㅎ', 'ㅊ',
+        'ㅍ', 'ㅈ', 'ㅆ', 'ㅉ',
+    ]
+
+    def get_chosungs(s: str):
+        chosungs = []
+        for w in list(s.strip()):
+            if '가' <= w <= '힣':
+                cho = (ord(w) - ord('가')) // 588
+                chosungs.append(CHOSUNG_LIST[cho])
+
+        return chosungs
+
+    py_serial = serial.Serial(
+        TTY,
+        BAUDRATE,
+    )
+
+
+    CONTROL.get()
     print(f'[*]   Control arduino...\n')
-    pass
+
+    while not stopped:
+        try:
+            text = src.get(timeout=TIMEOUT)
+        except queue.Empty:
+            continue
+
+        chosungs = get_chosungs(text)
+        high_freq_chosungs = list(set((c, CHOSUNG_TO_LIGHT_NUM[c])
+                                       for c in chosungs
+                                       if c in HIGH_FREQ_CHOSUNGS))
+        dprint(f'chosungs: {chosungs}')
+        dprint(f'high_freq_chosungs: {high_freq_chosungs}')
+
+        light_nums = [i[1] for i in high_freq_chosungs]
+
+        for n in light_nums:
+            py_serial.write(n.to_bytes(1, byteorder='big'))
+        py_serial.write(b'\xFF')
 
 
 @click.command()
